8d1d7bbe72d8207574ba56dc0084ea91c734120f
117503445 <t117503445@gmail.com>
2020-06-11 23:53:12 +0800

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
+# & "C:\User\App\docker-machine-Windows-x86_64\dm.exe" env ali | Invoke-Expression
 mvn clean
 mvn package
 docker rm smartlab -f
 docker build -t 117503445/smartlab .
-docker push 117503445/smartlab
-# docker run --name smartlab -d -p 8000:80 --restart=always 117503445/smartlab
+docker run --name smartlab -d -p 8007:80 --restart=always 117503445/smartlab
+# docker push 117503445/smartlab
