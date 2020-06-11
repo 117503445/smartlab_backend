dc33a15f2a9abc631b5215249ac278fc874cda80
117503445 <t117503445@gmail.com>
2020-06-11 22:56:02 +0800

    remove dockerfile

--- deploy.ps1
+++ deploy.ps1
@@ -3,4 +3,4 @@ mvn package
 docker rm smartlab -f
 docker build -t 117503445/smartlab .
 docker push 117503445/smartlab
-# docker run --name githubproxy -d -p 8000:80 --restart=always 117503445/smartlab
+# docker run --name smartlab -d -p 8000:80 --restart=always 117503445/smartlab
