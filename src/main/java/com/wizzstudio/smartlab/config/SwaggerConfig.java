4a04e7b0b68343fd0ff3fa26c893d648355b7630
117503445 <t117503445@gmail.com>
2020-06-14 16:22:43 +0800

    完善 swagger 支持

--- src/main/java/com/wizzstudio/smartlab/config/SwaggerConfig.java
+++ src/main/java/com/wizzstudio/smartlab/config/SwaggerConfig.java
@@ -2,13 +2,16 @@ package com.wizzstudio.smartlab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Import(SpringDataRestConfiguration.class)
 @Configuration
 public class SwaggerConfig {
     @Bean
--- src/main/java/com/wizzstudio/smartlab/repository/FeedBackRepository.java
+++ src/main/java/com/wizzstudio/smartlab/repository/FeedBackRepository.java
@@ -2,8 +2,10 @@ package com.wizzstudio.smartlab.repository;
 
 import com.wizzstudio.smartlab.entity.Feedback;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "feedback", path = "feedback")
 public interface FeedBackRepository extends CrudRepository<Feedback, Integer> {
+
 }
