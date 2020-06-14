5df148464e6440d5cce177952cb848a524671d39
117503445 <t117503445@gmail.com>
2020-06-14 16:50:00 +0800

    修复无法测试的问题

--- src/test/java/com/wizzstudio/smartlab/SmartlabApplicationTests.java
+++ src/test/java/com/wizzstudio/smartlab/SmartlabApplicationTests.java
@@ -2,12 +2,15 @@ package com.wizzstudio.smartlab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = SmartlabApplication.class)
 class SmartlabApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Test : contextLoads ");
     }
 
 }
