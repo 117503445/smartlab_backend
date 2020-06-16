ee9b0fcd3ed165e5855cd3f446efd21005514a40
117503445 <t117503445@gmail.com>
2020-06-16 21:46:02 +0800

    add build info

--- src/main/java/com/wizzstudio/smartlab/controller/ProjectInfoController.java
+++ src/main/java/com/wizzstudio/smartlab/controller/ProjectInfoController.java
@@ -1,17 +1,31 @@
 package com.wizzstudio.smartlab.controller;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping(path = "/")
 public class ProjectInfoController {
+    /**
+     * 当前版本
+     */
+    @Value("${app.version}")
+    private String version;
+    /**
+     * 打包时间
+     */
+    @Value("${app.build.time}")
+    private String buildTime;
+
+
     @GetMapping(path = "ver", produces = "application/json")
     public @ResponseBody
     String getAllRecords() {
-        return "1.2";
+        return String.format("{%s} {%s}", version, buildTime);
     }
 }
