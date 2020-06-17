d03f88aff7495981e3980a79bd1e6c0662b9a992
117503445 <t117503445@gmail.com>
2020-06-17 22:38:27 +0800

    修复 swagger

--- src/main/java/com/wizzstudio/smartlab/config/SwaggerConfig.java
+++ src/main/java/com/wizzstudio/smartlab/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.wizzstudio.smartlab.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,9 +15,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Import(SpringDataRestConfiguration.class)
 @Configuration
 public class SwaggerConfig {
+    @Value("${swaggerhost}")
+    private String swaggerHost;
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).host("smartlab.backend.117503445.top")
+        return new Docket(DocumentationType.SWAGGER_2).host(swaggerHost)
                 .pathMapping("/")
                 .select()
                 .apis(RequestHandlerSelectors.any())
